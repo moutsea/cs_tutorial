8e84606f958b184672403f26e15cf6d57201a25b
charles.yin <charles.yin@shopee.com>
2022-05-05 14:22:00 +0800

    add lab03

---
+++ cs61a/lab03/lab03.py
@@ -0,0 +1,46 @@
+""" Lab 3: Recursion and Midterm Review """
+
+def gcd(a, b):
+    """Returns the greatest common divisor of a and b.
+    Should be implemented using recursion.
+
+    >>> gcd(34, 19)
+    1
+    >>> gcd(39, 91)
+    13
+    >>> gcd(20, 30)
+    10
+    >>> gcd(40, 40)
+    40
+    """
+    "*** YOUR CODE HERE ***"
+    if a % b == 0:
+        return b
+    return gcd(b, a % b)
+
+def hailstone(n):
+    """Print out the hailstone sequence starting at n, and return the
+    number of elements in the sequence.
+
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    """
+    "*** YOUR CODE HERE ***"
+    def func(n, ans):
+        print(n)
+        if n == 1:
+            return ans
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n = n * 3 + 1
+        return func(n, ans+1) 
+    return func(n, 1)
---
+++ cs61a/lab03/lab03_extra.py
@@ -0,0 +1,129 @@
+""" Optional problems for Lab 3 """
+
+from lab03 import *
+
+## Higher order functions
+
+def cycle(f1, f2, f3):
+    """Returns a function that is itself a higher-order function.
+
+    >>> def add1(x):
+    ...     return x + 1
+    >>> def times2(x):
+    ...     return x * 2
+    >>> def add3(x):
+    ...     return x + 3
+    >>> my_cycle = cycle(add1, times2, add3)
+    >>> identity = my_cycle(0)
+    >>> identity(5)
+    5
+    >>> add_one_then_double = my_cycle(2)
+    >>> add_one_then_double(1)
+    4
+    >>> do_all_functions = my_cycle(3)
+    >>> do_all_functions(2)
+    9
+    >>> do_more_than_a_cycle = my_cycle(4)
+    >>> do_more_than_a_cycle(2)
+    10
+    >>> do_two_cycles = my_cycle(6)
+    >>> do_two_cycles(1)
+    19
+    """
+    "*** YOUR CODE HERE ***"
+
+## Lambda expressions
+
+def is_palindrome(n):
+    """
+    Fill in the blanks '_____' to check if a number
+    is a palindrome.
+
+    >>> is_palindrome(12321)
+    True
+    >>> is_palindrome(42)
+    False
+    >>> is_palindrome(2015)
+    False
+    >>> is_palindrome(55)
+    True
+    """
+    x, y = n, 0
+    f = lambda: y * 10 + x % 10
+    while x > 0:
+        x, y = x // 10, f()
+    return y == n
+
+## More recursion practice
+
+def skip_mul(n):
+    """Return the product of n * (n - 2) * (n - 4) * ...
+
+    >>> skip_mul(5) # 5 * 3 * 1
+    15
+    >>> skip_mul(8) # 8 * 6 * 4 * 2
+    384
+    """
+    if n <= 2:
+        return n
+    else:
+        return n * skip_mul(n - 2)
+
+def is_prime(n):
+    """Returns True if n is a prime number and False otherwise.
+
+    >>> is_prime(2)
+    True
+    >>> is_prime(16)
+    False
+    >>> is_prime(521)
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    def is_prime_helper(n, k):
+        if n == k:
+            return True
+        if n % k == 0:
+            return False
+        return is_prime_helper(n, k+1)
+    return is_prime_helper(n, 2)
+
+def interleaved_sum(n, odd_term, even_term):
+    """Compute the sum odd_term(1) + even_term(2) + odd_term(3) + ..., up
+    to n.
+
+    >>> # 1 + 2^2 + 3 + 4^2 + 5
+    ... interleaved_sum(5, lambda x: x, lambda x: x*x)
+    29
+    """
+    "*** YOUR CODE HERE ***"
+    def helper(n, odd_term, even_term):
+        if n == 0:
+            return odd_term, even_term(0)
+        f, tot = helper(n-1, odd_term, even_term)
+        next_f = odd_term if f == even_term else even_term
+        return next_f, tot + f(n)
+
+    _, ret = helper(n, odd_term, even_term)
+    return ret
+        
+
+def ten_pairs(n):
+    """Return the number of ten-pairs within positive integer n.
+
+    >>> ten_pairs(7823952)
+    3
+    >>> ten_pairs(55055)
+    6
+    >>> ten_pairs(9641469)
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    def helper(n, k):
+        if n < 10:
+            return n == k
+        return helper(n // 10, k) + (n % 10 == k)
+    
+    if n < 10:
+        return 0
+    return ten_pairs(n // 10) + helper(n // 10, 10 - n % 10)
