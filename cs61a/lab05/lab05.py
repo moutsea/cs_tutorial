860fcaaeee42658421c9da4f09be512464af7814
charles.yin <charles.yin@shopee.com>
2022-05-07 14:34:44 +0800

    add lab05

---
+++ cs61a/lab05/lab05.py
@@ -0,0 +1,163 @@
+""" Lab 05: Mutable Sequences and Trees """
+# Q2
+#from math import comb
+
+
+def map(fn, seq):
+    """Applies fn onto each element in seq and returns a list.
+
+    >>> map(lambda x: x*x, [1, 2, 3])
+    [1, 4, 9]
+    """
+    "*** YOUR CODE HERE ***"
+    return [fn(x) for x in seq]
+
+def filter(pred, seq):
+    """Keeps elements in seq only if they satisfy pred.
+
+    >>> filter(lambda x: x % 2 == 0, [1, 2, 3, 4])
+    [2, 4]
+    """
+    "*** YOUR CODE HERE ***"
+    return [x for x in seq if pred(x)]
+
+def reduce(combiner, seq):
+    """Combines elements in seq using combiner.
+
+    >>> reduce(lambda x, y: x + y, [1, 2, 3, 4])
+    10
+    >>> reduce(lambda x, y: x * y, [1, 2, 3, 4])
+    24
+    >>> reduce(lambda x, y: x * y, [4])
+    4
+    """
+    "*** YOUR CODE HERE ***"
+    if len(seq) == 1:
+        return seq[0]
+    ret = seq[0]
+    for i in range(1, len(seq)):
+        ret = combiner(ret, seq[i])
+    return ret
+
+# Q3
+def acorn_finder(t):
+    """Returns True if t contains a node with the value 'acorn' and
+    False otherwise.
+
+    >>> scrat = tree('acorn')
+    >>> acorn_finder(scrat)
+    True
+    >>> sproul = tree('roots', [tree('branch1', [tree('leaf'), tree('acorn')]), tree('branch2')])
+    >>> acorn_finder(sproul)
+    True
+    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+    >>> acorn_finder(numbers)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    if label(t) == 'acorn':
+        return True
+    for b in branches(t):
+        if acorn_finder(b):
+            return True
+    return False  
+
+# Q4
+def replace_leaf(t, old, new):
+    """Returns a new tree where every leaf value equal to old has
+    been replaced with new.
+
+    >>> yggdrasil = tree('odin',
+    ...                  [tree('balder',
+    ...                        [tree('thor'),
+    ...                         tree('loki')]),
+    ...                   tree('frigg',
+    ...                        [tree('thor')]),
+    ...                   tree('thor',
+    ...                        [tree('sif'),
+    ...                         tree('thor')]),
+    ...                   tree('thor')])
+    >>> laerad = copy_tree(yggdrasil) # copy yggdrasil for testing purposes
+    >>> print_tree(replace_leaf(yggdrasil, 'thor', 'freya'))
+    odin
+      balder
+        freya
+        loki
+      frigg
+        freya
+      thor
+        sif
+        freya
+      freya
+    >>> laerad == yggdrasil # Make sure original tree is unmodified
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if is_leaf(t) and label(t) == old:
+        return tree(new)
+    return tree(label(t), [replace_leaf(b, old, new) for b in branches(t)])
+    
+
+# Tree ADT
+def tree(label, branches=[]):
+    """Construct a tree with the given label value and a list of branches."""
+    for branch in branches:
+        assert is_tree(branch), 'branches must be trees'
+    return [label] + list(branches)
+
+def label(tree):
+    """Return the label value of a tree."""
+    return tree[0]
+
+def branches(tree):
+    """Return the list of branches of the given tree."""
+    return tree[1:]
+
+def is_tree(tree):
+    """Returns True if the given tree is a tree, and False otherwise."""
+    if type(tree) != list or len(tree) < 1:
+        return False
+    for branch in branches(tree):
+        if not is_tree(branch):
+            return False
+    return True
+
+def is_leaf(tree):
+    """Returns True if the given tree's list of branches is empty, and False
+    otherwise.
+    """
+    return not branches(tree)
+
+def print_tree(t, indent=0):
+    """Print a representation of this tree in which each node is
+    indented by two spaces times its depth from the root.
+
+    >>> print_tree(tree(1))
+    1
+    >>> print_tree(tree(1, [tree(2)]))
+    1
+      2
+    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+    >>> print_tree(numbers)
+    1
+      2
+      3
+        4
+        5
+      6
+        7
+    """
+    print('  ' * indent + str(label(t)))
+    for b in branches(t):
+        print_tree(b, indent + 1)
+
+def copy_tree(t):
+    """Returns a copy of t. Only for testing purposes.
+
+    >>> t = tree(5)
+    >>> copy = copy_tree(t)
+    >>> t = tree(6)
+    >>> print_tree(copy)
+    5
+    """
+    return tree(label(t), [copy_tree(b) for b in branches(t)])
